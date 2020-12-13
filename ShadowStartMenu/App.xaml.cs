9525dca3c1b7173cf732cf1fd639e1acafcc7861
Auros Nexus <aurosnex@gmail.com>
2020-12-13 07:15:26 -0500

    Implemented DI

--- ShadowStartMenu/App.xaml.cs
+++ ShadowStartMenu/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
 namespace ShadowStartMenu
 {
@@ -7,5 +9,27 @@ namespace ShadowStartMenu
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            IHostBuilder setup = CreateHostBuilder(e.Args);
+            IHost host = setup.Build();
+
+            Main main = (host.Services.GetService(typeof(Main)) as Main)!;
+            main.Show();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            Initialization initializer = new Initialization();
+            IHostBuilder builder = Host.CreateDefaultBuilder(args);
+            builder.ConfigureServices((env, services) =>
+            {
+                services.AddTransient<Main>();
+                initializer.Configure(services);
+            });
+            return builder;
+        }
     }
 }
---
+++ ShadowStartMenu/Initialization.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ShadowStartMenu
+{
+    public class Initialization
+    {
+        public void Configure(IServiceCollection services)
+        {
+            
+        }
+    }
+}
